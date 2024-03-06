43d67829ef49b11a8d395b35c0c714a487a5ebdc
Neil Hughes <neil.hughes@northcoders.com>
2024-03-06 14:33:42 +0000

    feat: Add Playlist and Song starter classes

---
+++ src/main/java/org/northcoders/Playlist.java
@@ -0,0 +1,26 @@
+package org.northcoders;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Playlist {
+    private List<Song> songs;
+
+    public Playlist() {
+        songs = new ArrayList<>();
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(songs);
+    }
+
+    public void play() {
+        for (int i = 0; i <= songs.size(); i++) {
+            System.out.println("Playing: " + songs.get(i).getTitle() + " by " + songs.get(i).getArtist());
+        }
+    }
+}
